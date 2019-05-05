3a4d4967683a9da3f9d2b2d3e2f55e545d42c20e
mupastir <mupastir@gmail.com>
2019-05-05 23:40:42 +0300

    Refactored all code and rewrited tests

--- chessdesk/chessdesk.py
+++ chessdesk/chessdesk.py
@@ -1,13 +1,21 @@
-from typing import List, Tuple
+from typing import List
 
 
 class ChessDesk:
-
     print_symbols: List[str]
 
     def __init__(self, height=None, weight=None):
         self.height, self.weight = height, weight
         self.print_symbols = ['*', ' ']
+        self.info_string = "There didn't put numbers, please enter to " \
+                           "create square desk HEIGHT.\n" \
+                           "To create rectangular desk " \
+                           "enter first HEIGHT and second WEIGHT. "
+        if self.are_normal_args():
+            self.desk = self.built_desk()
+            print(self.desk, end='')
+        else:
+            print(self.info_string, end='')
 
     def are_normal_args(self) -> bool:
         if self.weight is None:
@@ -19,19 +27,12 @@ class ChessDesk:
         else:
             return True
 
-    def __repr__(self):
+    def built_desk(self):
         row_to_print = ''
         for i in range(self.height):
             for j in range(self.weight):
-                row_to_print += (self.print_symbols[(i+j) % 2])
+                row_to_print += (self.print_symbols[(i + j) % 2])
             row_to_print += '\n'
         if self.height < 2 or self.weight < 2:
-            raise ValueError('Size too short!', [self.height, self.weight])
+            return f'Size too short!, [{self.height}, {self.weight}]'
         return row_to_print
-
-    def __call__(self):
-        if self.are_normal_args():
-            return self.__repr__()
-        else:
-            return {"There didn't put numbers, please enter to create square desk HEIGHT.\n"
-                  "To create rectangular desk enter first HEIGHT and second WEIGHT."}
--- chessdesk/test_chessdesk.py
+++ chessdesk/test_chessdesk.py
@@ -1,39 +1,45 @@
+import io
 import unittest
+import unittest.mock
 from chessdesk import ChessDesk
 
 
 class TestChessdesk(unittest.TestCase):
 
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def assert_stdout(self, required_string,
+                      mock_stdout, height=None, weight=None):
+        ChessDesk(height, weight)
+        self.assertEqual(mock_stdout.getvalue(), required_string)
+
     def test_no_argues_given(self):
-        required_string = ''''''
-        test = ChessDesk()
-        self.assertEqual(test(), required_string)
+        self.assert_stdout("There didn't put numbers, please enter to " 
+                           "create square desk HEIGHT.\n" 
+                           "To create rectangular desk " 
+                           "enter first HEIGHT and second WEIGHT. ")
 
     def test_height_only_given(self):
-        required_string = '''* * * \n * * *\n* * * \n * * *\n* * * \n * * *'''
-        test = ChessDesk(6)
-        self.assertEqual(test(), required_string)
+        self.assert_stdout("* * * \n * * *\n* * * "
+                           "\n * * *\n* * * \n * *"
+                           " *\n", height=6)
 
     def test_size_given_less_than_two(self):
-        required_string = {"'Size too short!' [1, 1]"}
-        test = ChessDesk(1)
-        self.assertEqual(test(), required_string)
+        self.assert_stdout("Size too short!, [1, 1]", height=1)
 
     def test_given_args_not_nums(self):
-        required_string = {"There didn't put numbers, please enter to create square desk HEIGHT.\n"
-                           "To create rectangular desk enter first HEIGHT and second WEIGHT."}
-        test = ChessDesk('str', 'str')
-        self.assertEqual(test(), required_string)
+        self.assert_stdout("There didn't put numbers, please enter to " 
+                           "create square desk HEIGHT.\n" 
+                           "To create rectangular desk " 
+                           "enter first HEIGHT and second WEIGHT. ",
+                           height='str', weight='str')
 
     def test_given_even_size(self):
-        required_string = '''* * * \n * * *\n* * * \n * * *'''
-        test = ChessDesk(4, 6)
-        self.assertEqual(test(), required_string)
+        self.assert_stdout("* * * \n * * *\n* * * \n * * *\n",
+                           height=4, weight=6)
 
     def test_given_odd_size(self):
-        required_string = '''* * * *\n * * * \n* * * *\n * * * \n* * * *'''
-        test = ChessDesk(5, 7)
-        self.assertEqual(test(), required_string)
+        self.assert_stdout("* * * *\n * * * \n* * * *\n * * * \n* * * *\n",
+                           height=5, weight=7)
 
 
 if __name__ == "__main__":
