a4768d8a2a226807486b861a9561f1bacf080466
mupastir <mupastir@gmail.com>
2019-05-10 14:12:33 +0300

    Envelop analysis refactor

--- envelopeanalysis/envelopanalysis.py
+++ envelopeanalysis/envelopanalysis.py
@@ -1,29 +1,30 @@
 class EnvelopAnalysis:
 
     def __init__(self):
-        self.info_msg = "Enter size of every side of 2 envelops in oreder:\n" \
+        self.INFO_MSG = "Enter size of every side of 2 envelops in oreder:\n" \
                         "1 - a-size\n" \
                         "1 - b-size\n" \
                         "2 - c-size\n" \
                         "2 - d-size\n\n"
-        self.err_msg = "There were put not numbers!"
-        print(self.info_msg, end='')
-        f_env_side_a, f_env_side_b, \
-            s_env_side_a, s_env_side_b = [input('Enter size of envelope: ')
-                                          for i in range(4)]
-        self.first_envelop = [f_env_side_a, f_env_side_b]
-        self.second_envelop = [s_env_side_a, s_env_side_b]
-
+        self.ERR_MSG = "There were put not numbers!"
+        print(self.INFO_MSG, end='')
+        self.first_envelop, self.second_envelop = self.input_sides()
         if self.are_normal_args():
-            print(f_env_side_a, f_env_side_b, s_env_side_a, s_env_side_b)
             if self.is_enclosed():
                 print('Envelope can be enclosed!\n')
             else:
                 print("Envelope can't be enclosed!\n")
             self.ask_restart()
         else:
-            print(self.err_msg)
+            print(self.ERR_MSG)
             self.ask_restart()
+
+    def input_sides(self):
+        sides = ['A', 'B']
+        envelops = 2
+        return [[input(f'Enter size {s} of envelope No{e+1}: ')
+                 for s in sides]
+                for e in range(envelops)]
 
     def are_normal_args(self) -> bool:
         try:
@@ -44,5 +45,4 @@ class EnvelopAnalysis:
         if min(self.first_envelop) > max(self.second_envelop) or \
                 min(self.second_envelop) > max(self.first_envelop):
             return True
-        else:
-            return False
+        return False
