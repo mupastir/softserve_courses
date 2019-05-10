5c91b8313042adcaf22c369855eb302df54a7ed0
mupastir <mupastir@gmail.com>
2019-05-10 18:08:24 +0300

    Add exception

--- chessdesk/chessdesk.py
+++ chessdesk/chessdesk.py
@@ -2,13 +2,14 @@ from typing import List
 
 
 class ChessDesk:
-
+            
     print_symbols: List[str]
 
     def __init__(self, height=None, weight=None):
         self.height, self.weight = height, weight
         self.print_symbols = ['*', ' ']
-        self.INFO_STRING = "There didn't put numbers, please enter to " \
+        self.INFO_STRING = "There didn't put numbers or values are too small" \
+                           ', please enter to ' \
                            "create square desk HEIGHT.\n" \
                            "To create rectangular desk " \
                            "enter first HEIGHT and second WEIGHT. "
@@ -21,12 +22,15 @@ class ChessDesk:
     def __repr__(self):
         print(self.desk, end='')
 
-    def are_normal_args(self) -> bool:
+    class SmallValues(Exception):
+        self.weight < 2 or self.height < 2
+
+    def are_normal_args(self, func) -> bool:
         if self.weight is None:
             self.weight = self.height
         try:
             int(self.height), int(self.weight)
-        except (ValueError, TypeError):
+        except (ValueError, TypeError, SmallValues):
             return False
         else:
             return True
@@ -37,6 +41,4 @@ class ChessDesk:
             for j in range(self.weight):
                 row_to_print += (self.print_symbols[(i + j) % 2])
             row_to_print += '\n'
-        if self.height < 2 or self.weight < 2:
-            return f'Size too short!, [{self.height}, {self.weight}]'
         return row_to_print
