7d9f22ff7d57c8dcf6cf317c01f9e251ad855068
mupastir <mupastir@gmail.com>
2019-05-11 16:29:44 +0300

    Right work of enclosing func

--- envelopeanalysis/envelopanalysis.py
+++ envelopeanalysis/envelopanalysis.py
@@ -1,26 +1,26 @@
 class EnvelopAnalysis:
 
     def __init__(self):
-        self.INFO_MSG = "Enter size of every side of 2 envelops in oreder:\n" \
+        self.INFO_MSG = "Enter size of every side of 2 envelops in order:\n" \
                         "1 - a-size\n" \
                         "1 - b-size\n" \
                         "2 - c-size\n" \
                         "2 - d-size\n\n"
         self.ERR_MSG = "There were put not numbers!"
+        self.result_msg = 'Envelope can be enclosed!\n'
         print(self.INFO_MSG, end='')
         self.first_envelop, self.second_envelop = self.input_sides()
         if self.are_normal_args():
             if self.is_enclosed():
-                print('Envelope can be enclosed!\n')
+                print(self.result_msg)
             else:
-                print("Envelope can't be enclosed!\n")
-            self.ask_restart()
+                print(self.result_msg.replace('can', 'can\'t'))
         else:
             print(self.ERR_MSG)
-            self.ask_restart()
+        self.ask_restart()
 
     def input_sides(self):
-        sides = ['A', 'B']
+        sides = ('A', 'B')
         envelops = 2
         return [[input(f'Enter size {s} of envelope No{e+1}: ')
                  for s in sides]
@@ -35,14 +35,25 @@ class EnvelopAnalysis:
         else:
             return True
 
+    def is_enclosed(self):
+        vertical_enclosed = \
+            min(self.first_envelop) > max(self.second_envelop) or \
+            min(self.second_envelop) > max(self.first_envelop)
+        horizontal_enclosed = \
+            min(self.first_envelop) > min(self.second_envelop) and \
+            max(self.first_envelop) > max(self.second_envelop) or \
+            min(self.second_envelop) > min(self.first_envelop) and \
+            max(self.second_envelop) > max(self.first_envelop)
+        if vertical_enclosed or horizontal_enclosed:
+            return True
+        return False
+
     def ask_restart(self):
         if input('If you want restart '
                  'calculation enter "y" or "yes": ').lower() == ('y' or 'yes'):
             EnvelopAnalysis()
         return
 
-    def is_enclosed(self):
-        if min(self.first_envelop) > max(self.second_envelop) or \
-                min(self.second_envelop) > max(self.first_envelop):
-            return True
-        return False
+
+if __name__ == "__main__":
+    EnvelopAnalysis()
