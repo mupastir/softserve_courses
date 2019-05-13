b16d404c7c32240b155ca3d924759dde97fc156f
mupastir <mupastir@gmail.com>
2019-05-13 12:05:37 +0300

    Add mode dicts

--- happyticket/happyticket.py
+++ happyticket/happyticket.py
@@ -1,7 +1,22 @@
 class HappyTicket:
 
-    def __init__(self):
+    def __init__(self, filepath: str, mode: str):
+        self.MODES = {"moskow": self.moscow_mode,
+                      "piter": self.peter_mode}
+        self.filename = filepath
+        self.result = {}
+        self.start = self.MODES[mode.lower()]
+        self.start()
+
+    def moscow_mode(self):
+        ...
+
+    def peter_mode(self):
         ...
 
     def __repr__(self):
         ...
+
+
+if __name__ == "__main__":
+    HappyTicket('text.txt', 'Piter')
