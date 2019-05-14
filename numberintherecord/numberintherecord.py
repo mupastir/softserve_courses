eda9b7a9c5c92c77da05de2153167e1b3ea343a6
mupastir <mupastir@gmail.com>
2019-05-15 01:17:46 +0300

    Num into record by dict

--- numberintherecord/numberintherecord.py
+++ numberintherecord/numberintherecord.py
@@ -1,112 +1,108 @@
+def suffix_nadcyat_under_14(unit_string):
+    return unit_string + "надцать"
+
+
+def suffix_nadcyat_under(unit_string):
+    return unit_string[:-1] + "надцать"
+
+
+ELEMENTARY = {0: "ноль", 1: "один", 2: "два", 3: "три", 4: "четыре",
+              5: "пять", 6: "шесть", 7: "семь", 8: "восемь", 9: "девять",
+              'exceptions': {0: "", 1: "одна", 2: "две", 10: "десять",
+                             40: "сорок", 90: "девяносто", 100: "сто",
+                             200: "двести", 300: "триста", 400: "четыреста"
+                             }}
+
+SUFFIX = dict((k, suffix_nadcyat_under_14) for k in range(11, 14))
+SUFFIX.update(dict((k, suffix_nadcyat_under) for k in range(14, 20)))
+SUFFIX.update(dict((k, "дцать") for k in range(20, 40, 10)))
+SUFFIX.update(dict((k, "десят") for k in range(50, 100, 10)))
+SUFFIX.update(dict((k, "сот") for k in range(500, 1000, 100)))
+
+THOUSAND = {1: "тысяча"}
+THOUSAND.update(dict((k, "тысячи") for k in range(2, 5)))
+THOUSAND.update(dict((k, "тысяч") for k in range(5, 20)))
+
+
 class NumberRecord:
 
-    def __init__(self, number):
-        self.number = int(number)
-        self.ELEMENTARY = {0: "ноль",
-                           1: "один",
-                           2: "два",
-                           3: "три",
-                           4: "четыре",
-                           5: "пять",
-                           6: "шесть",
-                           7: "семь",
-                           8: "восемь",
-                           9: "девять"}
-        self.UNITS = {10: "десять",
-                      12: "двенадцать",
-                      13: "тринадцать",
-                      40: "сорок",
-                      90: "девяносто",
-                      'suffix under 20': "надцать",
-                      'suffix under 40': "дцать",
-                      'suffix more 40': "десят"}
-        self.HUNDREDS = {100: "сто",
-                         200: "двести",
-                         300: "триста",
-                         400: "четыреста",
-                         'suffix till 900': "сот"}
-        self.THOUSAND = ["тысяча", "тысячи", "тысяч"]
+    def __init__(self, number: int):
+        self.number = number
         self.record = []
         self.transformation()
-        self.__repr__()
 
     def __repr__(self):
-        for e in self.record:
-            print(e, end=' ')
+        return self.record
 
     def transformation(self):
-        if self.number == 0:
-            self.record.append(self.ELEMENTARY[0])
+        if self.number < 10:
+            self.record.append(ELEMENTARY[self.number])
         else:
-            self.transform_basic_third()
+            self._transform_basic_third()
             while self.number % 1000 > 0:
-                self.append_thousand_name()
-                self.transform_basic_third()
+                self._append_thousand_name()
+                self._transform_basic_third()
+        self.record = self._list_to_string(self.record)
+        print(self.record)
+        return self.record
 
-    def transform_basic_third(self):
-        self.units(self.number % 10)
-        self.hundreds()
+    @staticmethod
+    def _list_to_string(our_list):
+        return " ".join(map(str, our_list))
+
+    def _transform_basic_third(self):
+        self._units(self.number % 10)
+        self._hundreds()
         self.number //= 10
         if '' in self.record:
             self.record.remove('')
 
-    def append_thousand_name(self):
-        if self.number % 100 == 11:
-            self.record.insert(0, self.THOUSAND[2])
-        elif self.number % 10 == 1:
-            self.record.insert(0, self.THOUSAND[0])
-        elif 1 < self.number % 10 < 5:
-            self.record.insert(0, self.THOUSAND[1])
+    def _append_thousand_name(self):
+        if self.number % 100 in THOUSAND:
+            self.record.insert(0, THOUSAND[self.number % 100])
         else:
-            self.record.insert(0, self.THOUSAND[2])
+            self.record.insert(0, THOUSAND[self.number % 10])
 
-    def units(self, unit: int):
-        if len(self.record) > 2 and unit == 1:
-            unit_list = [unit, "одна"]
-        elif unit == 2:
-            unit_list = [unit, "две"]
+    def _units(self, unit: int):
+        if len(self.record) > 2 and unit in ELEMENTARY['exceptions']:
+            unit_list = [unit, ELEMENTARY['exceptions'][unit]]
         elif unit == 0:
             unit_list = [unit, ""]
         else:
-            unit_list = [unit, self.ELEMENTARY[unit]]
-        return self.tens(unit_list)
+            unit_list = [unit, ELEMENTARY[unit]]
+        return self._tens(unit_list)
 
-    def tens(self, units: list):
-        digit_to_check = self.get_digit()
+    def _tens(self, units: list):
+        digit_to_check = self._get_digit()
+        tens_number_to_check = int(str(digit_to_check) + str(units[0]))
         if digit_to_check == 0:
-            self.record.insert(0, self.ELEMENTARY[units[0]])
-        elif digit_to_check == 1 and units[0] == 0:
-            self.record.insert(0, self.UNITS[10])
-        elif digit_to_check == 1 and units[0] < 4:
-            self.record.insert(0, units[1] +
-                               self.UNITS['suffix under 20'])
-        elif digit_to_check == 1:
-            self.record.insert(0, units[1][:-1] +
-                               self.UNITS['suffix under 20'])
-        elif digit_to_check < 4:
-            self.record.insert(0, self.ELEMENTARY[digit_to_check] +
-                               self.UNITS['suffix under 40'])
-            self.record.insert(1, units[1])
-        elif digit_to_check == 4:
-            self.record.insert(0, self.UNITS[40])
-            self.record.insert(1, units[1])
-        elif digit_to_check == 9:
-            self.record.insert(0, self.UNITS[90])
-            self.record.insert(1, units[1])
-        elif digit_to_check > 4:
-            self.record.insert(0, self.ELEMENTARY[digit_to_check] +
-                               self.UNITS['suffix more 40'])
-            self.record.insert(1, units[1])
+            self.record.insert(0, units[1])
+        elif tens_number_to_check in ELEMENTARY['exceptions']:
+            self.record.insert(0,
+                               ELEMENTARY['exceptions'][tens_number_to_check])
+        elif tens_number_to_check in SUFFIX:
+            self.record.insert(0, SUFFIX[tens_number_to_check](units[1]))
+        elif digit_to_check * 10 in ELEMENTARY['exceptions']:
+            self.record.insert(0,
+                               ELEMENTARY['exceptions'][digit_to_check * 10] +
+                               ' ' + units[1])
+        elif digit_to_check * 10 in SUFFIX:
+            self.record.insert(0, ELEMENTARY[digit_to_check] +
+                               SUFFIX[digit_to_check * 10] + ' ' + units[1])
 
-    def hundreds(self):
-        hundred = 100
-        digit_to_check = self.get_digit()
-        if 0 < digit_to_check < 5:
-            self.record.insert(0, self.HUNDREDS[digit_to_check*hundred])
-        elif digit_to_check > 4:
-            self.record.insert(0, self.ELEMENTARY[digit_to_check] +
-                               self.HUNDREDS['suffix till 900'])
+    def _hundreds(self):
+        digit_to_check = self._get_digit()
+        hundred_to_check = digit_to_check * 100
+        if hundred_to_check in ELEMENTARY['exceptions']:
+            self.record.insert(0, ELEMENTARY['exceptions'][hundred_to_check])
+        else:
+            self.record.insert(0, ELEMENTARY[digit_to_check] +
+                               SUFFIX[hundred_to_check])
 
-    def get_digit(self):
+    def _get_digit(self):
         self.number //= 10
         return self.number % 10
+
+
+if __name__ == "__main__":
+    NumberRecord(555666)
