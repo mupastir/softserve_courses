62f41dde603edbd3747e2d72eda03f32670a419c
mupastir <mupastir@gmail.com>
2019-05-15 01:43:29 +0300

    Chessdesk refactored

--- chessdesk/chessdesk.py
+++ chessdesk/chessdesk.py
@@ -1,37 +1,34 @@
 from typing import List
 
+PRINT_SYMBOLS = ('*', ' ')
+INFO_STRING = "There didn't put numbers or values are too small" \
+              ', please enter to ' \
+              "create square desk HEIGHT.\n" \
+              "To create rectangular desk " \
+              "enter first HEIGHT and second WEIGHT. "
+
 
 class ChessDesk:
-            
-    print_symbols: List[str]
 
     def __init__(self, height=None, weight=None):
         self.height, self.weight = height, weight
-        self.print_symbols = ['*', ' ']
-        self.INFO_STRING = "There didn't put numbers or values are too small" \
-                           ', please enter to ' \
-                           "create square desk HEIGHT.\n" \
-                           "To create rectangular desk " \
-                           "enter first HEIGHT and second WEIGHT. "
-        if self.are_normal_args():
-            self.desk = self.built_desk()
-        else:
-            self.desk = self.INFO_STRING
-        self.__repr__()
-
-    def __repr__(self):
-        print(self.desk, end='')
-
-    def are_normal_args(self) -> bool:
         if self.weight is None:
             self.weight = self.height
-        try:
+        if self._are_normal_args():
             self.height, self.weight = int(round(self.height)), \
                                        int(round(self.weight))
+            self.desk = self.built_desk()
+
+    def __repr__(self):
+        return self.desk
+
+    def _are_normal_args(self) -> bool:
+        try:
+            int(round(self.height)), int(round(self.weight))
             if self.weight < 2 or self.height < 2:
                 raise ValueError
         except (ValueError, TypeError):
-            return False
+            print(INFO_STRING)
         else:
             return True
 
@@ -39,6 +36,6 @@ class ChessDesk:
         row_to_print = ''
         for i in range(self.height):
             for j in range(self.weight):
-                row_to_print += (self.print_symbols[(i + j) % 2])
+                row_to_print += (PRINT_SYMBOLS[(i + j) % 2])
             row_to_print += '\n'
         return row_to_print
