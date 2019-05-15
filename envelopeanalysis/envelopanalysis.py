2a323c28ab994c9554b0e8b17f0adcc1a484fdb6
mupastir <mupastir@gmail.com>
2019-05-15 09:48:40 +0300

    Envelop analysis refactored with tests

--- envelopeanalysis/envelopanalysis.py
+++ envelopeanalysis/envelopanalysis.py
@@ -1,59 +1,86 @@
+INFO_MSG = "Enter size of every side of 2 envelops in order:\n" \
+           "1 - a-size\n" \
+           "1 - b-size\n" \
+           "2 - c-size\n" \
+           "2 - d-size\n\n"
+ERR_MSG = "There were put not numbers or not existing envelops!\n" \
+          "(Less or equal zero!)"
+
+
+def _input_sides():
+    print(INFO_MSG, end='')
+    sides = ('A', 'B')
+    envelops = 2
+    return [[input(f'Enter size {s} of envelope No{e+1}: ')
+             for s in sides]
+            for e in range(envelops)]
+
+
 class EnvelopAnalysis:
 
-    def __init__(self):
-        self.INFO_MSG = "Enter size of every side of 2 envelops in order:\n" \
-                        "1 - a-size\n" \
-                        "1 - b-size\n" \
-                        "2 - c-size\n" \
-                        "2 - d-size\n\n"
-        self.ERR_MSG = "There were put not numbers!"
+    def __init__(self, envelop_first, envelop_second):
         self.result_msg = 'Envelope can be enclosed!\n'
-        self.first_envelop, self.second_envelop = [(), ()]
+        self.first_envelop, self.second_envelop = envelop_first, envelop_second
 
     def main(self):
-        print(self.INFO_MSG, end='')
-        self.first_envelop, self.second_envelop = self.input_sides()
-        self.check_data(self.first_envelop, self.second_envelop)
-        self.ask_restart()
+        if self.check_data() is None:
+            self._restart()
+        print(self.result_msg)
+        self._ask_restart()
 
-    def check_data(self, first_envelop, second_envelop):
-        if self.are_normal_args(first_envelop, second_envelop):
-            if self.is_enclosed(first_envelop, second_envelop):
-                print(self.result_msg, end='')
-            else:
-                print(self.result_msg.replace('can', 'can\'t'), end='')
+    def check_data(self):
+        if self._are_normal_args(self.first_envelop, self.second_envelop):
+            self.first_envelop, self.second_envelop = \
+                [float(self.first_envelop[0]), float(self.first_envelop[1])], \
+                [float(self.second_envelop[0]), float(self.second_envelop[1])]
+            if not self._is_enclosed(self.first_envelop, self.second_envelop):
+                self.result_msg = self.result_msg.replace('can', 'can\'t')
         else:
-            print(self.ERR_MSG, end='')
+            return
+        return self.result_msg
 
-    def input_sides(self):
-        sides = ('A', 'B')
-        envelops = 2
-        return [[input(f'Enter size {s} of envelope No{e+1}: ')
-                 for s in sides]
-                for e in range(envelops)]
+    def __repr__(self):
+        return self.result_msg
 
-    def are_normal_args(self, first_envelop, second_envelop) -> bool:
+    def _are_normal_args(self, first_envelop, second_envelop) -> bool:
         try:
             float(first_envelop[0]), float(first_envelop[1])
             float(second_envelop[0]), float(second_envelop[1])
-        except (ValueError, TypeError):
+            if float(first_envelop[0]) <= 0 or \
+                    float(first_envelop[1]) <= 0 or \
+                    float(second_envelop[0]) <= 0 or \
+                    float(second_envelop[1]) <= 0:
+                raise ValueError
+        except (TypeError, ValueError):
+            print(ERR_MSG)
             return False
         else:
             return True
 
-    def is_enclosed(self, first_envelop, second_envelop):
+    @staticmethod
+    def _is_enclosed(first_envelop, second_envelop):
         enclosed = \
-            min(first_envelop) > min(second_envelop) and \
-            max(first_envelop) > max(second_envelop) \
+            (min(first_envelop) > min(second_envelop) and
+             max(first_envelop) > max(second_envelop))\
             or \
-            min(second_envelop) > min(first_envelop) and \
-            max(second_envelop) > max(first_envelop)
+            (min(second_envelop) > min(first_envelop) and
+             max(second_envelop) > max(first_envelop))
         if enclosed:
             return True
         return False
 
-    def ask_restart(self):
+    def _ask_restart(self):
         if input('If you want restart '
                  'calculation enter "y" or "yes": ').lower() == ('y' or 'yes'):
-            EnvelopAnalysis()
+            self._restart()
         return
+
+    @staticmethod
+    def _restart():
+        first_envelop, second_envelop = _input_sides()
+        EnvelopAnalysis(first_envelop, second_envelop).main()
+
+
+if __name__ == "__main__":
+    first_envelop, second_envelop = _input_sides()
+    EnvelopAnalysis(first_envelop, second_envelop).main()
--- envelopeanalysis/test_envelopanalysis.py
+++ envelopeanalysis/test_envelopanalysis.py
@@ -1,28 +1,26 @@
-import io
-import unittest.mock
+import unittest
 from envelopanalysis import EnvelopAnalysis
 
 
 class TestEnvelop(unittest.TestCase):
-    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
-    def assert_stdout(self, required_string,
-                      side1a, side1b, side2a, side2b,
-                      mock_stdout):
-        test = EnvelopAnalysis()
-        test.check_data((side1a, side1b), (side2a, side2b))
-        self.assertEqual(mock_stdout.getvalue(), required_string)
 
     def test_norm_enclosed_args(self):
-        self.assert_stdout('Envelope can be enclosed!\n',
-                           5, 5.5, 2, 2.5)
+        self.assertEqual(EnvelopAnalysis([5, 5.5], [2, 2.5]).check_data(),
+                         'Envelope can be enclosed!\n')
 
     def test_norm_non_enclosed_args(self):
-        self.assert_stdout('Envelope can\'t be enclosed!\n',
-                           5, 5.5, 7.5, 2.5)
+        self.assertEqual(EnvelopAnalysis([5, 5.5], [7.5, 2.5]).check_data(),
+                         'Envelope can\'t be enclosed!\n')
 
     def test_non_normal_args(self):
-        self.assert_stdout("There were put not numbers!",
-                           'miyke', 5.5, 7.5, 2.5)
+        self.assertRaises(TypeError,
+                          EnvelopAnalysis(['miyke', 5.5],
+                                          [7.5, 2.5]).check_data())
+
+    def test_args_less_equal_zero(self):
+        self.assertRaises(ValueError,
+                          EnvelopAnalysis([-1, 5.5],
+                                          [7.5, 0]).check_data())
 
 
 if __name__ == "__main__":
