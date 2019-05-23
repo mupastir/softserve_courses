3dcb12381c6a44ee4b609944578b6888ae2194fb
mupastir <mupastir@gmail.com>
2019-05-23 13:11:31 +0300

    Refactored by PEP8

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -7,21 +7,25 @@ class FibonacciRange:
 
     def build_sequence(self):
         if self._start_range < 0 and self._end_range <= 0:
-            self._result_sequence = self._negative_sequence(
-                end_range=self._start_range, start_range=self._end_range)
+            self._result_sequence \
+                = self._negative_sequence(end_range=self._start_range,
+                                          start_range=self._end_range)
         elif self._start_range < 0 < self._end_range:
-            self._result_sequence = self._negative_sequence(
-                end_range=self._start_range, start_range=1) + \
-                                    self._positive_sequence(self._end_range,
-                                                            start_range=0)
+            self._result_sequence \
+                = (self._negative_sequence(end_range=self._start_range,
+                                           start_range=1)
+                   + self._positive_sequence(self._end_range,
+                                             start_range=0))
         else:
-            self._result_sequence = self._positive_sequence(self._end_range,
-                                                            self._start_range)
+            self._result_sequence \
+                = self._positive_sequence(self._end_range,
+                                          self._start_range)
         return self._result_sequence
 
     def _negative_sequence(self, end_range, start_range=0) -> list:
-        negative_sequence = self._positive_sequence(start_range=-start_range,
-                                                    end_range=-end_range)
+        negative_sequence \
+            = self._positive_sequence(start_range=-start_range,
+                                      end_range=-end_range)
         for i in range(len(negative_sequence)):
             if i % 2 == 1:
                 negative_sequence[i] = -negative_sequence[i]
